05a5b0d31b425799f9b0745d4ede1c3d674d4e71
wangzehua1202 <wangzehua1202@foxmail.com>
2017-09-27 15:35:56 +0800

    6-1-1
    用户注册接口初步

--- Web/iTalker/src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
+++ Web/iTalker/src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -1,6 +1,7 @@
 package net.qiujuer.web.italker.push.service;
 
 
+import net.qiujuer.web.italker.push.bean.api.account.RegisterModel;
 import net.qiujuer.web.italker.push.bean.db.User;
 
 import javax.ws.rs.*;
@@ -12,27 +13,17 @@ import javax.ws.rs.core.MediaType;
 public class AccountService {
 
     /**
-     * GET 127.0.0.1/api/account/login
-     * @return
-     */
-    @GET
-    @Path("/login")
-    public String get(){
-        return "you get the login";
-    }
-
-    /**
      * POST 127.0.0.1/api/account/login
      * @return
      */
     @POST
-    @Path("/login")
+    @Path("/register")
     //指定请求和返回的响应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User post(){
+    public User register(RegisterModel model){
         User user = new User();
-        user.setName("佩琪");
+        user.setName(model.getName());
         user.setSex(24);
         return user;
     }
