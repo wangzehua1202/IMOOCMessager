8a9f159169f3896490e60b6430903631cfdc6615
wangzehua1202 <wangzehua1202@foxmail.com>
2017-10-19 10:19:22 +0800

    8-1
    客户端card和db相关类新增

--- App/ITalker/factory/src/main/java/com/xingsu/italker/factory/model/card/UserCard.java
+++ App/ITalker/factory/src/main/java/com/xingsu/italker/factory/model/card/UserCard.java
@@ -6,22 +6,28 @@ import com.xingsu.italker.factory.model.db.User;
 import java.util.Date;
 
 /**
- * Created by Administrator on 2017/9/30 0030.
+ * 用户卡片，用于接收服务器返回
+ * @author qiujuer Email:qiujuer@live.cn
+ * @version 1.0.0
  */
-
-public class UserCard implements Author{
+public class UserCard implements Author {
     private String id;
     private String name;
     private String phone;
-
     private String portrait;
     private String desc;
     private int sex = 0;
 
+    // 用户关注人的数量
     private int follows;
+
+    // 用户粉丝的数量
     private int following;
+
+    // 我与当前User的关系状态，是否已经关注了这个人
     private boolean isFollow;
 
+    // 用户信息最后的更新时间
     private Date modifyAt;
 
     public String getId() {
@@ -104,30 +110,24 @@ public class UserCard implements Author{
         this.modifyAt = modifyAt;
     }
 
-    //缓存一个对应的User
-    //不能被Gson框架解析，所以过滤掉
+    // 缓存一个对应的User, 不能被GSON框架解析使用ø
     private transient User user;
 
-    public User build(){
-        if(user == null){
+    public User build() {
+        if (user == null) {
             User user = new User();
-
             user.setId(id);
             user.setName(name);
-            user.setPhone(phone);
-
             user.setPortrait(portrait);
+            user.setPhone(phone);
             user.setDesc(desc);
             user.setSex(sex);
-
+            user.setFollow(isFollow);
             user.setFollows(follows);
             user.setFollowing(following);
-            user.setFollow(isFollow);
-
             user.setModifyAt(modifyAt);
             this.user = user;
         }
         return user;
-
     }
 }
--- App/ITalker/factory/src/main/java/com/xingsu/italker/factory/model/db/AppDatabase.java
+++ App/ITalker/factory/src/main/java/com/xingsu/italker/factory/model/db/AppDatabase.java
@@ -4,10 +4,12 @@ import com.raizlabs.android.dbflow.annotation.Database;
 
 /**
  * 数据库的基本信息
- * Created by Administrator on 2017/9/30 0030.
+ *
+ * @author qiujuer Email:qiujuer@live.cn
+ * @version 1.0.0
  */
-@Database(name=AppDatabase.NAME, version = AppDatabase.VERSION)
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION)
 public class AppDatabase {
     public static final String NAME = "AppDatabase";
-    public static final int VERSION = 1;
+    public static final int VERSION = 2;
 }
