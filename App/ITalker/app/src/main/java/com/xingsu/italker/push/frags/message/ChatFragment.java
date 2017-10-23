cda4e4c565b8951af5be6a81fc7ccc5f771a1fdb
wangzehua1202 <wangzehua1202@foxmail.com>
2017-10-23 10:24:41 +0800

    9-12
    客户端聊天布局1

--- App/ITalker/app/src/main/java/com/xingsu/italker/push/frags/message/ChatFragment.java
+++ App/ITalker/app/src/main/java/com/xingsu/italker/push/frags/message/ChatFragment.java
@@ -2,20 +2,75 @@ package com.xingsu.italker.push.frags.message;
 
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.xingsu.italker.common.common.app.Fragment;
+import com.xingsu.italker.common.common.widget.PortaitView;
+import com.xingsu.italker.push.R;
 import com.xingsu.italker.push.activites.MessageActivity;
+
+import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public abstract class ChatFragment extends Fragment {
+public abstract class ChatFragment extends Fragment
+    implements AppBarLayout.OnOffsetChangedListener{
 
     private String mReceiverId;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.appbar)
+    AppBarLayout mAppBarLayout;
 
     @Override
     protected void initArgs(Bundle bundle) {
         super.initArgs(bundle);
         mReceiverId = bundle.getString(MessageActivity.KEY_RECEIVER_ID);
+    }
+
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+
+        initToolbar();
+        initAppbar();
+
+        //RecyclerView 基本设置
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void initToolbar(){
+        Toolbar toolbar = mToolbar;
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+    }
+
+
+    //给appbar设置一个监听，得到关闭和打开的时候的进度
+    private void initAppbar() {
+        mAppBarLayout.addOnOffsetChangedListener(this);
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        if(verticalOffset == 0){
+
+        }
     }
 }
--- App/ITalker/app/src/main/java/com/xingsu/italker/push/frags/message/ChatUserFragment.java
+++ App/ITalker/app/src/main/java/com/xingsu/italker/push/frags/message/ChatUserFragment.java
@@ -1,15 +1,23 @@
 package com.xingsu.italker.push.frags.message;
 
 
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
+import com.xingsu.italker.common.common.widget.PortaitView;
 import com.xingsu.italker.push.R;
 
+import butterknife.BindView;
+
 /**
+ * 用户聊天界面
  * A simple {@link Fragment} subclass.
  */
 public class ChatUserFragment extends ChatFragment {
 
+    @BindView(R.id.im_portrait)
+    PortaitView mPortaitView;
 
     public ChatUserFragment() {
         // Required empty public constructor
@@ -20,4 +28,34 @@ public class ChatUserFragment extends ChatFragment {
         return R.layout.fragment_chat_user;
     }
 
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        super.onOffsetChanged(appBarLayout, verticalOffset);
+        View view = mPortaitView;
+        if(verticalOffset == 0){
+            //完全展开
+            mPortaitView.setVisibility(View.VISIBLE);
+            view.setScaleX(1);
+            view.setScaleY(1);
+            view.setAlpha(1);
+        }else{
+            //绝对值运算
+            verticalOffset = Math.abs(verticalOffset);
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            if(verticalOffset >= totalScrollRange){
+                //关闭状态
+                mPortaitView.setVisibility(View.INVISIBLE);
+                view.setScaleX(0);
+                view.setScaleY(0);
+                view.setAlpha(0);
+            }else{
+                //中间状态
+                float progress = 1 - verticalOffset/(float)totalScrollRange;
+                mPortaitView.setVisibility(View.VISIBLE);
+                view.setScaleX(progress);
+                view.setScaleY(progress);
+                view.setAlpha(progress);
+            }
+        }
+    }
 }
